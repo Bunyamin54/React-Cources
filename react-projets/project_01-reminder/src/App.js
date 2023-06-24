74d4eaaecfb62e54582b1a13f3de91864f945fa4
Bunyamin Genc <bunyaming@uia.no>
2023-06-24 15:43:17 +0200

    lessonCard

--- react-projets/project_01-reminder/src/App.js
+++ react-projets/project_01-reminder/src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
+import Header from './Header';
+import Lesson from './Lesson';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+   <> 
+    <Header/> 
+    <Lesson/>
+    
+
+
+    </>
   );
 }
 
--- react-projets/project_01-reminder/src/Header.jsx
+++ react-projets/project_01-reminder/src/Header.jsx
@@ -3,9 +3,9 @@
 
 const Header = () => {
   return (
-    <nav class="navbar bg-body-tertiary">
-  <div class="container">
-    <a class="navbar-brand" href="#">
+    <nav className="navbar bg-body-tertiary">
+  <div className="container">
+    <a className="navbar-brand" href="#">
       <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24"/>
     </a>
   </div>
