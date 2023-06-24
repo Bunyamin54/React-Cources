4db297b91edc044cf05a8c5a7035baaaf407c6a9
Bunyamin Genc <bunyaming@uia.no>
2023-06-24 15:37:13 +0200

    div navbar

---
+++ react-projets/project_01-reminder/src/Header.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+
+const Header = () => {
+  return (
+    <nav class="navbar bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24"/>
+    </a>
+  </div>
+</nav>
+  )
+}
+
+export default Header
---
+++ react-projets/project_01-reminder/src/Lesson.jsx
@@ -0,0 +1,21 @@
+import LessonCard from "./LessonCard";
+
+
+
+const Lesson = () => {
+  return (
+    <>
+      <LessonCard/>
+      <LessonCard/>
+      <LessonCard/>
+      <LessonCard/>
+      <LessonCard/>
+      <LessonCard/>
+
+
+
+    </>
+  )
+}
+
+export default Lesson
