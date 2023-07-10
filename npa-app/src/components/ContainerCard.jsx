407fc94327b3e4d3c57c3026ed8e9379970f203a
Bunyamin Genc <bunyaming@uia.no>
2023-07-10 20:35:47 +0200

    nba projects

---
+++ npa-app/src/components/ContainerCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Form from 'react-bootstrap/Form';
+
+const ContainerCard = () => {
+  return (
+    <>
+    
+    <Form.Control type="text" placeholder="Search Player..." />
+    
+    
+    
+    </>
+
+
+
+  )
+}
+
+export default ContainerCard
