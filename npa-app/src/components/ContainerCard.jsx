89cc88db5c1a02400ce15debbba837ea849c3be3
Bunyamin Genc <bunyaming@uia.no>
2023-07-10 20:53:12 +0200

    asyncron

--- npa-app/src/components/ContainerCard.jsx
+++ npa-app/src/components/ContainerCard.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 const ContainerCard = () => {
+
+  const [search,setSearch] = useState("")
+  
+ const handleChange = (e) => {
+ 
+  setSearch(e.target.value)   //! setter methodu aseyncron olarak calisir. 
+
+ }
+
   return (
     <>
-      <Form.Control type="search" placeholder="Search Player..." />
+      <Form.Control type="search" placeholder="Search Player..." onChange={handleChange} />
       <Container className='p-3 rounded-4 card-container my-3'>
         <Row xs={2} md={4} lg={6} className='justifey-content-center g-3'>
           <Col>1 of 2</Col>
