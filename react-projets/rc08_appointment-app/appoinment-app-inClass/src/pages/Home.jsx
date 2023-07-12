44c1ab89ca512062e9e603735257910f50bd4de4
Bunyamin Genc <bunyaming@uia.no>
2023-07-12 22:02:01 +0200

    new fixed

--- react-projets/rc08_appointment-app/appoinment-app-inClass/src/pages/Home.jsx
+++ react-projets/rc08_appointment-app/appoinment-app-inClass/src/pages/Home.jsx
@@ -1,17 +1,16 @@
-import { useState } from "react"
 import AppointmentList from "../components/AppointmentList"
 import Doctors from "../components/Doctors"
 import { appointmentData } from "../helper/data"
 import { useState } from "react"
 
 const Home = () => {
+  const [appointments, setAppointments] = useState(appointmentData)
 
-  const[appointments, setAppointments] = useState(appointmentData)
-
+  console.log(appointments)
   return (
     <main className="text-center mt-2">
       <h1 className="display-5 text-danger">CLARUS HOSPITAL</h1>
-      <Doctors />
+      <Doctors apps={appointments} setApps={setAppointments} />
       <AppointmentList apps={appointments} />
     </main>
   )
