b86a5ca0d99e7646b1b482cc50811a601f866fa0
Bunyamin Genc <bunyaming@uia.no>
2023-07-12 19:15:31 +0200

    react icons

--- react-projets/rc08_appointment-app/appoinment-app-inClass/src/App.js
+++ react-projets/rc08_appointment-app/appoinment-app-inClass/src/App.js
@@ -1,8 +1,11 @@
 import "./App.css"
 import Home from "./pages/Home"
-
+// import{  AiFillDelete} from "react-icons/ai"
 const App = () => {
-  return <Home />
+  return  <div>
+ <Home />
+ {/* <AiFillDelete className="text-danger fs-3" /> */}
+  </div>
 }
 
 export default App
