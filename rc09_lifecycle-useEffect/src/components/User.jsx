2d61ca200370c591bb3edc9091c8754d480ed3f3
Bunyamin Genc <bunyaming@uia.no>
2023-07-13 22:03:01 +0200

    useEffect

--- rc09_lifecycle-useEffect/src/components/User.jsx
+++ rc09_lifecycle-useEffect/src/components/User.jsx
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from "react"
 
 const User = () => {
-   let userData = ""
-  fetch("https://randomuser.me/api/")
-  .then((res) => res.json())
-  .then((data)=> (userData = data))
-  .catch((err)=> console.log(err))
+  // let userData = "deneme"
+
+  const [userData, setUserData] = useState()
+
+  const getUser = () => {
+    fetch("https://randomuser.me/api")
+      .then((res) => res.json())
+      .then((data) => setUserData(data.results[0]))
+      .catch((err) => console.log(err))
+  }
+
+  useEffect(() => {
+    // fetch("https://randomuser.me/api")
+    //   .then((res) => res.json())
+    //   .then((data) => setUserData(data.results[0]))
+    //   .catch((err) => console.log(err))
+    getUser()
+  }, [])
+
   console.log(userData)
   return (
     <div>
-   <img src="" alt="" />
-   <h4>Hi My name is</h4>
-   <h1>Name</h1>
-   <h3>Email</h3>
-   <h4>dob</h4>
-   <h5>phone</h5>
-   <h6>address</h6>
+      {/* Optional Chaining */}
+      <img src={userData?.picture?.large} className="rounded-circle" alt="" />
+      <h4>Hi, My Name is</h4>
+      <h1>
+        {userData?.name?.first} {userData?.name?.last}
+      </h1>
+      <h3>{userData?.email}</h3>
+      <h4>{new Date(userData?.dob?.date).toLocaleDateString()}</h4>
+      <h5>{userData?.phone}</h5>
+      <h6>{userData?.location?.city}</h6>
+
+      <button className="btn btn-success" onClick={getUser}>
+        Get Random User
+      </button>
     </div>
   )
 }
