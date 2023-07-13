e4f16681e9d67edd62b3d452bd987646def0f8cc
Bunyamin Genc <bunyaming@uia.no>
2023-07-13 21:29:21 +0200

    fetch data

--- rc09_lifecycle-useEffect/src/components/User.jsx
+++ rc09_lifecycle-useEffect/src/components/User.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
 const User = () => {
+   let userData = ""
+  fetch("https://randomuser.me/api/")
+  .then((res) => res.json())
+  .then((data)=> (userData = data))
+  .catch((err)=> console.log(err))
+  console.log(userData)
   return (
-    <div>User</div>
+    <div>
+   <img src="" alt="" />
+   <h4>Hi My name is</h4>
+   <h1>Name</h1>
+   <h3>Email</h3>
+   <h4>dob</h4>
+   <h5>phone</h5>
+   <h6>address</h6>
+    </div>
   )
 }
 
