781f364f2c9b7b7ad5241086291431667c59223a
Bunyamin Genc <bunyaming@uia.no>
2023-07-13 20:39:40 +0200

    useEffect

--- rc09_lifecycle-useEffect/src/App.js
+++ rc09_lifecycle-useEffect/src/App.js
@@ -1,9 +1,18 @@
-import LifeCycleMethods from "./components/LifeCycleMethods"
+// import LifeCycleMethods from "./components/LifeCycleMethods"
+import { useState } from "react"
+import UseEffectHook from "./components/UseEffectHook"
 
 function App() {
+  const [visible, setVisible] = useState(false)
   return (
     <div className="container text-center mt-4">
-      <LifeCycleMethods />
+      <button className="btn btn-danger" onClick={() => setVisible(!visible)}>
+        Show/Hide
+      </button>
+
+      {/* {visible && <LifeCycleMethods />} */}
+
+      {visible && <UseEffectHook />}
     </div>
   )
 }
