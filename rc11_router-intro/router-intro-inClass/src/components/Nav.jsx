308f03f5b0b449766fd5c32460ba1fab483a03ee
Bunyamin Genc <bunyaming@uia.no>
2023-07-19 20:26:48 +0200

    add link and useNagitave

--- rc11_router-intro/router-intro-inClass/src/components/Nav.jsx
+++ rc11_router-intro/router-intro-inClass/src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
@@ -7,10 +7,10 @@ const Nav = () => {
         <Link to="/">Home</Link>
       </li>
       <li>
-        <Link to ="/instructors">Instructors</Link>
+        <NavLink to ="/instructors">Instructors</NavLink>
       </li>
       <li>
-        < Link to ="/contact">Contact</Link>
+        <NavLink to ="/contact">Contact</NavLink>
       </li>
     </ul>
   );
