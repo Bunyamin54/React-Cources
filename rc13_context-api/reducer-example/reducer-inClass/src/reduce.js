38cc7ff132c363552af85822e696a852ebe82ed9
Bunyamin Genc <bunyaming@uia.no>
2023-07-22 13:06:27 +0200

    reducer

---
+++ rc13_context-api/reducer-example/reducer-inClass/src/reduce.js
@@ -0,0 +1,27 @@
+ export const initialState = {
+    loading: false,
+    catImage: "",
+    error:"",
+}
+
+export const reducer = (initialState, action) => { 
+    
+    switch (action.type) {
+    case "START":
+    return {...initialState , loading: true}
+    
+    case "SUCCESS":
+        return {...initialState , catImage:action.payload, error: ""}
+
+    case "FAIL":
+   return {...initialState , catImage:"", error:action.payload}
+    
+    default:
+        break;
+  }
+
+}
+
+//? action
+//! {type: "SUCCESS", payload: catImage}
+//! {type: "START"}
