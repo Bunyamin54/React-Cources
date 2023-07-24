72a4bdeb9172c9c34df6366cb5b5a1f2717d9ea0
Bunyamin Genc <bunyaming@uia.no>
2023-07-24 19:42:40 +0200

    styled-components

--- rc14_styled-comps/styled-comp-intro/src/App.js
+++ rc14_styled-comps/styled-comp-intro/src/App.js
@@ -1,10 +1,11 @@
-import styled from 'styled-components'
+
+import { Button } from './components/Buton.styled';
 
 const App = () => {
   return (
     <>
       <h1>Styled Components</h1>
-      
+      <Button>Tikla</Button>
     </>
   );
 };
---
+++ rc14_styled-comps/styled-comp-intro/src/components/Buton.styled.jsx
@@ -0,0 +1,12 @@
+import styled  from "styled-components";
+
+export const Button = styled.button `
+
+background-color: hotpink;
+color:white;
+border: 2px solid hotpink;
+font-size:1.3rem;
+padding: 0.7rem  3rem;
+
+`
+
