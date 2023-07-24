8c4997ee10aa67709b5a2ec0ce22288c3485fd46
Bunyamin Genc <bunyaming@uia.no>
2023-07-24 20:58:47 +0200

    Link style

--- rc14_styled-comps/styled-comp-intro/src/App.js
+++ rc14_styled-comps/styled-comp-intro/src/App.js
@@ -1,10 +1,16 @@
-import { Button, DarkButton } from "./components/Buton.styled"
+import { Button, DarkButton, PinkButton } from "./components/Buton.styled"
 import Flex from "./components/Flex"
+import StyledLink from "./components/Links"
 import Title from "./components/Title"
 
 const App = () => {
   return (
     <>
+      <Flex>
+        <StyledLink href="https://www.clarusway.com">Clarusway</StyledLink>
+        <StyledLink react href="https://www.react.dev">React</StyledLink>
+      </Flex>
+
       <Flex bg="#bebe">
         <Title renk="white" bgRenk="red">
           Styled Components
@@ -13,12 +19,15 @@ const App = () => {
         <DarkButton>Send</DarkButton>
       </Flex>
 
-      <Title renk = "purple">Flexbox</Title>
-     
-      <Title bgRenk = "green" renk= "white">Flexbox</Title>
-
+      <Title renk="purple">Flexbox</Title>
+      <Title bgRenk="green">Flexbox</Title>
       <Flex bg="tomato">
         <h1>PROPS</h1>
+      </Flex>
+
+      <Flex>
+        <PinkButton>Pembe</PinkButton>
+        <PinkButton primary>İçi boş</PinkButton>
       </Flex>
     </>
   )
--- rc14_styled-comps/styled-comp-intro/src/components/Flex.jsx
+++ rc14_styled-comps/styled-comp-intro/src/components/Flex.jsx
@@ -1,15 +1,15 @@
-import styled from 'styled-components';
+import styled from "styled-components"
 
 const Flex = styled.div`
-display:flex;
-flex-direction:column;
-align-items:center;
-gap:1rem;
-max-width: 1000px;
-margin:auto;
-background-color: ${ (props) => props.bg || "transparent"};
-/* background-color: ${ (bg) => bg || "transparent"};  //? kisa yontem  icerik de bg varsa aktarir */
- 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  max-width: 500px;
+  margin: 1rem auto;
+  /* background-color: ${(props) => props.bg || "transparent"}; */
+  background-color: ${({ bg }) => bg ?? "transparent"};
 `
 
-export default Flex;
+export default Flex
