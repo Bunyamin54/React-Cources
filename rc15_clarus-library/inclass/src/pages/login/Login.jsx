65ce4aaed0a4abee413dc3c4e05e707674ec6e57
Bunyamin Genc <bunyaming@uia.no>
2023-07-26 22:10:43 +0200

    login main

--- rc15_clarus-library/inclass/src/pages/login/Login.jsx
+++ rc15_clarus-library/inclass/src/pages/login/Login.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { FormContainer, Header, LoginContainer, StyledButton, StyledForm, StyledInput } from './Login.style'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <LoginContainer>
+      <FormContainer>
+        <Header>Login Here</Header>
+        <StyledForm>
+          <StyledInput/>
+          <StyledInput/>
+          <StyledButton>Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
   )
 }
 
