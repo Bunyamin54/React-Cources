da90086e9cb92dc167906cfb0e979fde1e687ac3
Bunyamin Genc <bunyaming@uia.no>
2023-07-26 19:28:41 +0200

    theme style

--- rc15_clarus-library/inclass/src/styles/theme.js
+++ rc15_clarus-library/inclass/src/styles/theme.js
@@ -1,1 +1,34 @@
-//create theme
+export const lightTheme = {
+    colors: {
+      navbarBgColor: "white",
+      mainColor: "#86C8BC",
+      logoColor: "darkslategray",
+      linkColor: "#8B7E74",
+      detailBgColor: "#e1f1dd",
+      detailColor: "rgb(5,5,5)",
+      linkHoverColor: "rgb(5,5,5)",
+      headerFormColor: "rgba(15, 63, 42, 0.36)",
+    },
+
+   screens: {
+
+     lg: "992px",
+      
+   }
+
+  };
+
+  export const darkTheme = {
+
+    colors: {
+        navbarBgColor: "white",
+        mainColor: "#86C8BC",
+        logoColor: "darkslategray",
+        linkColor: "#8B7E74",
+        detailBgColor: "#e1f1dd",
+        detailColor: "rgb(5,5,5)",
+        linkHoverColor: "rgb(5,5,5)",
+        headerFormColor: "rgba(15, 63, 42, 0.36)",
+      },
+
+  }
