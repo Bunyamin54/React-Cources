49a0659484abc6210c2998e2390a0bd478922c3d
Bunyamin Genc <bunyaming@uia.no>
2023-08-03 21:21:01 +0200

    text filed

--- rc17-18_movie-app/mui-intro-vite/src/App.jsx
+++ rc17-18_movie-app/mui-intro-vite/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
 
 import './App.css'
 import Typo from './components/Typo'
+import TextFieldComp from './components/TextFieldComp'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Typo/>
+      <TextFieldComp/>
     </>
   )
 }
---
+++ rc17-18_movie-app/mui-intro-vite/src/components/TextFieldComp.jsx
@@ -0,0 +1,22 @@
+import TextField  from '@mui/material'
+import Container from '@mui/material/Container'
+
+
+const TextFieldComp = () => {
+  return (
+    <Container  sx={{ mt: 4}}  maxWidth="md" >
+    <TextField id="email"
+     label="Email" 
+    variant="outlined" 
+     placeholder="Enter your email"
+     required
+     fullWidth
+     margin="dence"
+    />
+     <TextField id="filled-basic" label="Filled" variant="filled" />
+    <TextField id="standard-basic" label="Standard" variant="standard" />
+    </Container>
+  )
+}
+
+export default TextFieldComp
