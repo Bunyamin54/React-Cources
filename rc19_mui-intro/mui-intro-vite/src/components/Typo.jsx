c64ba72da1488bc88d9f802a605f4084ef0cfc03
Bunyamin Genc <bunyaming@uia.no>
2023-08-05 23:27:49 +0200

    container box componets i mui

--- rc19_mui-intro/mui-intro-vite/src/components/Typo.jsx
+++ rc19_mui-intro/mui-intro-vite/src/components/Typo.jsx
@@ -4,22 +4,28 @@ import Typography from "@mui/material/Typography"  //? burda sadece mui materili
 
 import Button from "@mui/material/Button"
 
+import Box from "@mui/material/Box"
+import Container from "@mui/material/Container"
+
 const Typo = () => {
   return (
-    <div>
+
+    <Container maxWidth= "sm"> 
+    <Box sx= {{padding: "1rem" , display: "flex", flexDirection: "column", gap:"1rem", border: "2px solid tomato"}}>
       <Typography variant="h3" component= "h5" color="red">
         MUI TYPO
       </Typography>
 
-      <Typography variant="subtitle1"  color="blue">
+      <Typography variant="subtitle1"  color="blue" mt={2}>
         MUI TYPO
       </Typography>
 
-      <Typography variant="button">
+      <Typography variant="button" sx={{color: "red", backgroundColor: "yellow", mt:"1rem", }}  align="center">
         MUI TYPO
       </Typography>
 
-    </div>
+    </Box>
+    </Container>
   )
 }
 
