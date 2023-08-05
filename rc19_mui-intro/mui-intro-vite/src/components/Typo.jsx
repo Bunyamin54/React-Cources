4c49db418f2235d8e8108973bf997b1c240cd02e
Bunyamin Genc <bunyaming@uia.no>
2023-08-05 15:46:22 +0200

    typo button

--- rc19_mui-intro/mui-intro-vite/src/components/Typo.jsx
+++ rc19_mui-intro/mui-intro-vite/src/components/Typo.jsx
@@ -1,7 +1,8 @@
 // import {Typography} from "@mui/material"
 
-import Typography from "@mui/material"  //? burda sadece mui materilin tamamini yuklemen cikariyor
+import Typography from "@mui/material/Typography"  //? burda sadece mui materilin tamamini yuklemen cikariyor
 
+import Button from "@mui/material/Button"
 
 const Typo = () => {
   return (
