ea056c325dd34a30c84647d4fdb8f31b45b57ea3
Bunyamin Genc <bunyaming@uia.no>
2023-08-12 16:57:37 +0200

    auth

--- rc22-26_stock-app/stock-app-starter1/src/services/authApiCall.js
+++ rc22-26_stock-app/stock-app-starter1/src/services/authApiCall.js
@@ -1,19 +1,27 @@
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
-
-
+import { useDispatch } from "react-redux"
+import {fetchFail, fetchStart,loginSuccess} from "../features/authSlice"
 export const login =  async (userData) => {
 
-   const navigate = useNavigate ()
-    console.log(navigate)
-   const BASE_URL = "https://14113.fullstack.clarusway.com/"
- try {
-   const {data} = await axios.post(`${BASE_URL}/account/auth/login/`,userData)
-    console.log(data)
+const navigate =useNavigate()
+const  dispatch =useDispatch()
 
+console.log(dispatch)
+
+
+   const BASE_URL = "https://14113.fullstack.clarusway.com/"
+   dispatch(fetchStart())
+   try {
+   const {data} = await axios.post(`${BASE_URL}/account/auth/login/`,userData)
+ 
+
+    dispatch(loginSuccess(data))
+    toastSuccesNotify("Login islemi basarili")
     navigate ("/stock")
 } catch (error) {
     console.log(error)
+    dispatch(fetchFail())
  }
 
 }
