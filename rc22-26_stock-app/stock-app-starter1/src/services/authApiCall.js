0e5a956cc2c866490883a1dda4abcaa39f01f706
Bunyamin Genc <bunyaming@uia.no>
2023-08-12 16:49:30 +0200

    useNavigate

--- rc22-26_stock-app/stock-app-starter1/src/services/authApiCall.js
+++ rc22-26_stock-app/stock-app-starter1/src/services/authApiCall.js
@@ -1,10 +1,17 @@
 import axios from "axios"
+import { useNavigate } from "react-router-dom"
+
 
 export const login =  async (userData) => {
+
+   const navigate = useNavigate ()
+    console.log(navigate)
    const BASE_URL = "https://14113.fullstack.clarusway.com/"
  try {
    const {data} = await axios.post(`${BASE_URL}/account/auth/login/`,userData)
     console.log(data)
+
+    navigate ("/stock")
 } catch (error) {
     console.log(error)
  }
