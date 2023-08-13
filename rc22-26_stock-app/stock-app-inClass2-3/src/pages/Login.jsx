5cef3b4b3dfaa72507b7d674ca21c254721f3573
Bunyamin Genc <bunyaming@uia.no>
2023-08-13 17:01:34 +0200

    env devolopment

--- rc22-26_stock-app/stock-app-inClass2-3/src/pages/Login.jsx
+++ rc22-26_stock-app/stock-app-inClass2-3/src/pages/Login.jsx
@@ -4,38 +4,33 @@ import Grid from "@mui/material/Grid"
 import Typography from "@mui/material/Typography"
 import LockIcon from "@mui/icons-material/Lock"
 import image from "../assets/result.svg"
-import { Link, useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 import Box from "@mui/material/Box"
 import TextField from "@mui/material/TextField"
-import { Button } from "@mui/material"
-import {Formik, Form} from "formik"
-import { object, string, } from 'yup';
-import { login } from "../hooks/authApiCall"
-
-
+import Button from "@mui/material/Button"
+import { Formik, Form } from "formik"
+import { object, string } from "yup"
+// import { login } from "../hooks/useAuthCall"
+import useAuthCall from "../hooks/useAuthCall"
 
 const Login = () => {
-  const navigate = useNavigate()
+  const { login } = useAuthCall()
 
-   console.log(navigate)
-
+  //? harici validasyon şemasi
   const loginSchema = object({
-   
     email: string()
-    .email("Lutfen valid bir email giriniz.!")
-    .required("Bu alan zorunludur"),
-    password: string("Lutfen valid bir email giriniz.!").required("Bu alan zorunludur")
-    .min(8, "En az 8 karakter girilmelidir.")
-    .max(16, "En fazla 16 karakter girilmelidir.")
-    .matches(/\d+/, "En az bir rakam icermelidir")
-    .matches(/[a-z]/, "En az bir kucuk harf icermelidir")
-    .matches(/ [A-Z]/, "En az bir buyuk harf icermelidir")
-    .matches(/ [!,?{}<>%&$#£+-.]/, "En az bir ozel karakter icermelidir")
+      .email("Lutfen valid bir email giriniz")
+      .required("Bu alan zorunludur"),
+    password: string()
+      .required("Bu alan zorunludur")
+      .min(8, "En az 8 karakter girilmelidir")
+      .max(16, "En fazla 16 karakter girilmelidir")
+      .matches(/\d+/, "En az bir rakam içermelidir.")
+      .matches(/[a-z]/, "En az bir küçük harf içermelidir.")
+      .matches(/[A-Z]/, "En az bir büyük harf içermelidir.")
+      .matches(/[!,?{}><%&$#£+-.]+/, "En az bir özel karekter içermelidir."),
+  })
 
-  });
-
-
-  
   return (
     <Container maxWidth="lg">
       <Grid
